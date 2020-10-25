7bc9dc1b385c766e50b2468e8712b6dbade8dc38
Matthew May <mattryanmay@gmail.com>
2020-10-25 18:05:39 -0500

    Center page text

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mx-auto pb-32 flex-grow flex justify-center items-center">
+    <div class="container mx-auto pb-32 flex-grow flex justify-center items-center text-center">
       <div>
         <p>I'm working on something better for this page.</p>
         <p>Oh, none of the links work yet. Getting to that soon!</p>
