87fd05dc2f05781fed48c58f78741cc117f4f549
Matthew May <mattryanmay@gmail.com>
2020-10-25 17:56:56 -0500

    Update temp-page text

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,7 @@
     <div class="container mx-auto pb-32 flex-grow flex justify-center items-center">
       <div>
         <p>I'm working on something better for this page.</p>
+        <p>Oh, none of the links work yet. Getting to that soon!</p>
       </div>
     </div>
 </template>
