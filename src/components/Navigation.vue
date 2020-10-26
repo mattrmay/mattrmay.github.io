7331a99c8c7afcb5ffab1b2336b41a3f97145c79
Matthew May <mattryanmay@gmail.com>
2020-10-25 23:15:34 -0500

    Fix sizing on sm and greater viewports

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,7 +6,7 @@
             <path v-if="menuOpen" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
         </svg>
         </button>
-        <nav :class="menuOpen ? 'block' : 'hidden'" class="absolute left-0 w-full bg-white sm:bg-none sm:relative sm:block mt-4 pt-4 pb-8 sm:pt-2 sm:pb-0 sm:flex sm:space-x-6 space-y-4 sm:space-y-0 sm:items-center text-3xl sm:text-base font-thin sm:font-normal tracking-wide text-center sm:text-left shadow-md sm:shadow-none">
+        <nav :class="menuOpen ? 'block' : 'hidden'" class="absolute left-0 w-full bg-white sm:bg-none sm:relative sm:block mt-4 pt-4 pb-8 sm:mt-0 sm:pt-2 sm:pb-0 sm:flex sm:space-x-6 space-y-4 sm:space-y-0 sm:items-center text-3xl sm:text-base font-thin sm:font-normal tracking-wide text-center sm:text-left shadow-md sm:shadow-none">
             <a class="block" href="#">About</a>
             <a class="block" href="#">Updates</a>
             <a class="block" href="#">Projects</a>
