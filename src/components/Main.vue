05b49b055eec2e34dc3e910135e7405d48863218
Matthew May <mattryanmay@gmail.com>
2020-10-25 23:16:57 -0500

    Merge from dev

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="flex-grow border border-1 border-red-500">
-  </div>
+    <div class="container mx-auto pb-32 flex-grow flex justify-center items-center text-center">
+      <div>
+        <p>I'm working on something better for this page.</p>
+        <p>Oh, none of the links work yet. Getting to that soon!</p>
+      </div>
+    </div>
 </template>
 
 <script>
