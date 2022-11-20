752a5b638f99746b536082df6124d264587008d3
Matthew May <1563750+mattrmay@users.noreply.github.com>
2022-11-19 23:51:05 -0600

    update temp page text.

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,7 +2,6 @@
     <div class="container mx-auto pb-32 flex-grow flex justify-center items-center text-center">
       <div>
         <p>I'm working on something better for this page.</p>
-        <p>Oh, none of the links work yet. Getting to that soon!</p>
       </div>
     </div>
 </template>
